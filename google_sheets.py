63d5cf93d50d07696a563396781f84651ed5fb39
Tuva <tuva.bjornberg02@gmail.com>
2024-10-03 17:54:30 +0200

    add encoding

--- google_sheets.py
+++ google_sheets.py
@@ -5,7 +5,7 @@ import os
 def fetch_google_emails_csv(file_path):
     records = []
 
-    with open(file_path, mode="r") as file:
+    with open(file_path, mode="r", encoding="utf-8") as file:
         reader = csv.reader(file)
         for row in reader:
             
